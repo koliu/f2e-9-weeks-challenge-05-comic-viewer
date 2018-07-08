f0387d1379d7c509cb16b8fe53a089a164063abb
ko <ko.liu.learn@gmail.com>
2018-07-08 21:38:03 +0800

    Implement nav-bar of reading

--- src/components/reading.vue
+++ src/components/reading.vue
@@ -6,12 +6,19 @@
   .container.bg-black-10
     img.banner(v-if="!loggedIn", src="/src/static/assets/ad-1.png", alt="Vue", title="Vue")
     .header-bar
-      .nav {{chapter}}
+      .nav
+        i.f-title3 My Hexschool
+        i.f-title3.fas.fa-caret-right
+        .selection
+          select.f-sub-title(v-model="selectedChapterId")
+            option(v-for="(item, index) in chapters" :value="item.id") {{item.title}}
+          select.f-sub-title(v-model="selectedPage")
+            option(v-for="(item, index) in selectedChapter.images" :value="index") Page {{index + 1}}
       .view-mode
         i.fas.fa-sun
         input(id="viewMode" type="checkbox" name="viewMode" v-model="isDark")
         label(for="viewMode")
-        i.fas.fa-moon
+        i.far.fa-moon
     img.banner(v-if="!loggedIn", src="/src/static/assets/ad-2.png", alt="Bootstrap 4", title="Bootstrap 4")
     .main
       .content
@@ -24,13 +31,17 @@
 </template>
 <script>
 import SessionStorage from "../js/session-storage.js";
+import Data from "../data/data.js";
 
 export default {
   data() {
     return {
       loggedIn: false,
-      chapter: "",
-      isDark: false
+      isDark: false,
+      chapters: [],
+      selectedChapter: {},
+      selectedChapterId: "",
+      selectedPage: 0
     };
   },
   methods: {
@@ -43,10 +54,29 @@ export default {
       this.loginManager.logout();
       this.loggedIn = this.loginManager.isLoggedIn();
       console.log("reading", "logout", this.loggedIn);
+    },
+    findChapter(key) {
+      console.log("key", key, Data.chapters);
+      return Data.chapters.find(e => e.id === key);
+    },
+    loadData() {
+      this.chapters = Data.chapters;
+      this.selectedChapter = this.findChapter(this.$route.params.chapter);
+      this.selectedChapterId = this.selectedChapter
+        ? this.selectedChapter.id
+        : "";
+      this.selectedPage = 0;
+    }
+  },
+  watch: {
+    selectedChapterId() {
+      this.navigator.pushTo(this.selectedChapterId);
+      this.loadData();
     }
   },
   created() {
-    this.chapter = this.$route.params.chapter;
+    this.loadData();
+    console.log("findChapter", this.data);
 
     this.loggedIn = this.loginManager.isLoggedIn();
     console.log("reading", "created", this.loggedIn);
@@ -101,12 +131,108 @@ export default {
   }
 }
 
+.header-bar {
+  align-items: center;
+  display: flex;
+  height: 35px;
+  justify-content: space-between;
+  margin: 24px 0;
+  transition: background-color 0.5s ease-in 0.2s, color 0.5s ease 0.2s;
+  width: 620px;
+
+  .nav {
+    align-items: center;
+    display: flex;
+    line-height: 35px;
+
+    i {
+      padding-right: 8px;
+    }
+
+    select {
+      border: 2px solid $color-black;
+      display: inline-block;
+      height: 35px;
+      width: 127px;
+
+      & option:checked {
+        background-color: $color-black;
+        color: $color-white;
+      }
+    }
+  }
+  .view-mode {
+    display: flex;
+    align-items: center;
+
+    i {
+      font-size: 20px;
+      padding-left: 8px;
+    }
+
+    input {
+      display: none;
+
+      &:checked + label:after {
+        right: -2px;
+      }
+    }
+
+    label {
+      background-color: transparent;
+      border: 2px solid $color-black;
+      box-sizing: border-box;
+      cursor: pointer;
+      display: inline-block;
+      height: 24px;
+      position: relative;
+      width: 48px;
+      transition: border-color 0.5s ease-in 0.2s,
+        background-color 0.5s ease-in 0.2s;
+
+      &::after {
+        background-color: $color-black;
+        border: 2px solid $color-black;
+        box-sizing: border-box;
+        content: "";
+        height: 24px;
+        position: absolute;
+        right: 22px;
+        top: -2px;
+        width: 24px;
+        transition: right 0.2s ease, border-color 0.5s ease-in 0.2s,
+          background-color 0.5s ease-in 0.2s;
+      }
+    }
+  }
+}
+
+#app,
+.header {
+  transition: background-color 0.5s ease-in 0.2s, color 0.5s ease 0.2s;
+}
 .dark-mode {
   background-color: $color-black;
 
   .header {
     background-color: $color-green;
     color: $color-black;
+  }
+
+  .header-bar {
+    i {
+      color: $color-white;
+    }
+
+    label {
+      border-color: $color-white;
+      background-color: $color-black;
+
+      &::after {
+        background-color: $color-white;
+        border-color: $color-white;
+      }
+    }
   }
 }
 </style>
---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+const chapter1 = {
+  id: "chapter1",
+  title: "Chapter 1",
+  desc: "The F2E Challenge Start!",
+  new: false,
+  images: []
+};
+const chapter2 = {
+  id: "chapter2",
+  title: "Chapter 2",
+  desc: "Todo List is Going Crazy!",
+  new: true,
+  images: []
+};
+for (let i = 0; i < 12; ) {
+  chapter1.images[i] = `../static/assets/storyboard-${++i}.png`;
+}
+for (let i = 0; i < 6; ) {
+  chapter2.images[i] = `../static/assets/storyboard-${++i}.png`;
+}
+
+export default {
+  chapters: [chapter1, chapter2]
+};
