f4c8d2d8c4d28367d0472667eb326c0ded4ad97e
ko <ko.liu.learn@gmail.com>
2018-07-10 22:33:54 +0800

    Use webpack-content-replacer-plugin to replace path src to dist in bundle.js

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,6 +4,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import VueLoaderPlugin from "vue-loader/lib/plugin";
 import webpack from "webpack";
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+import ContentReplacerWebpackPlugin from 'webpack-content-replacer-plugin';
 
 // the path(s) that should be cleaned
 const pathsToClean = [
@@ -105,7 +106,19 @@ export default (module = {
       copyUnmodified: false,
       debug: 'debug', // 'debug','warning'
       ignore: []
+    }),
+
+    new ContentReplacerWebpackPlugin({
+      modifiedFile: `${path.resolve(__dirname, "dist")}/bundle.js`,
+      modifications: [{
+        regex: /\"\/src\//g,
+        modification: '"',
+      }, {
+        regex: /\.\.\/src\//g,
+        modification: '../dist/',
+      }],
     })
+
   ],
   devServer: {
     // Display only errors to reduce the amount of output.
